43be9783f800093c3e3f2a0188f3cc65e8722608
Solen Dogan <solendogan@gmail.com>
2021-05-16 19:11:37 +0300

    added other widgets

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,7 @@ class MainApp extends StatelessWidget {
 
 class RealWorldApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _RealWorldAppState("REAL WORLD APP");
-  }
+  State<StatefulWidget> createState() => _RealWorldAppState("REAL WORLD APP");
 }
 
 class _RealWorldAppState extends State<RealWorldApp> {
@@ -33,12 +31,13 @@ class _RealWorldAppState extends State<RealWorldApp> {
   Future<List<dynamic>> _fetchData() async {
     final String apiURL = 'https://randomuser.me/api/?results=10';
     var parsedUserList;
+    var randomUserList;
     //call the random API
     var response = await http.get(
       Uri.parse(apiURL),
     );
     if (response.statusCode == 200) {
-      var randomUserList = jsonDecode(response.body)["results"];
+      randomUserList = jsonDecode(response.body)["results"];
       parsedUserList = randomUserList
           .map((randomUser) => User.fromJSON(randomUser))
           .toList();
@@ -72,28 +71,58 @@ class _RealWorldAppState extends State<RealWorldApp> {
         ),
       ]),
       body: Center(
-        child: _isLoading == true
-            ? CircularProgressIndicator(
-                backgroundColor: Colors.amber,
-              )
-            : ListView.builder(
-                itemCount: this._finalUserList != null
-                    ? this._finalUserList.length
-                    : 0,
-                padding: EdgeInsets.all(8),
-                itemBuilder: (context, i) {
-                  return Column(children: [
-                    Image.network(
-                        (this._finalUserList[i] as User).picture.large),
-                    Text((this._finalUserList[i] as User).fullName),
-                    Divider(
-                      color: Colors.amber,
-                      height: 25.0,
-                    )
-                  ]);
-                },
-              ),
+          child: _isLoading == true
+              ? CircularProgressIndicator(
+                  backgroundColor: Colors.amber,
+                )
+              : VideoList(userList: _finalUserList)),
+    );
+  }
+}
+
+class VideoList extends StatelessWidget {
+  final userList;
+
+  VideoList({@required this.userList});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.userList != null ? this.userList.length : 0,
+      itemBuilder: (context, i) {
+        User currentUser = this.userList[i];
+        return Column(children: [
+          VideoCell(user: currentUser,),
+          Container(
+            height: 6.0,
+          ),
+          Divider(
+            color: Colors.amber,
+            height: 25.0,
+          ),
+        ]);
+      },
+    );
+  }
+}
+
+class VideoCell extends StatelessWidget {
+  final User user;
+
+  VideoCell({ @required this.user});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Image.network(user.picture.large),
+          Text(
+            user.fullName,
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
+      padding: EdgeInsets.all(16.0),
     );
   }
 }
